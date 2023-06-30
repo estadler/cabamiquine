76004eb5daf452b4f633eb8bee70f2dc41fe0ff3
Eva Stadler <68209845+estadler@users.noreply.github.com>
2023-06-30 12:16:25 +1000

    Delete Cabamiquine directory

--- Cabamiquine/helper_functions/general-helper-functions.R
+++
@@ -1,6 +0,0 @@
-
-# geometric mean for rows of a data.frame ---------------------------------
-
-row_geom_mean <- function(data){
-  apply(data, 1, function(x) ifelse(sum(x==0)==ncol(data),0,exp(mean(log(x)))))
-}
