7dff2207fdc0f6856b6c6bcad7cb6f751393ebc0
Eva Stadler <68209845+estadler@users.noreply.github.com>
2023-06-30 12:23:38 +1000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Merck (cabamiquine) project
+
+Collaborative project with Merck.
+
+Key features include:
+
+- `driver.R` which can run the analysis from start to finish
+- `setup.R` which loads all libraries and other variables used throughout
+- `.gitignore` which list files and directories which should be ignored by `git`
+- `raw-data/` which includes all raw data from experiments
+- `processing/` which hold scripts to clean and process data before analysis
+- `analysis/` which hold scripts used to analyse data and generate outputs
+- `output/` which holds outputs. This directory should be added to the `.gitignore` so outputs are not stored in the `git` history
+- `helper functions` which holds any custom functions used throughout the project
+
---
+++ setup.R
@@ -0,0 +1,39 @@
+# -------------------------------------------------------------------------
+#' This file is used to set up everything that's needed across the project
+#' It loads libraries, creates functions, sets themes and defaults
+#' 
+# -------------------------------------------------------------------------
+
+
+# load packages -----------------------------------------------------------
+
+library("readxl")
+library("writexl")
+library("reshape2")
+library("ggplot2")
+library("viridis")
+
+# load helper functions ----------------------------------------------------
+
+source("helper_functions/estimates-from-data-helpers.R")
+source("helper_functions/deterministic-model-functions.R")
+source("helper_functions/stochastic-model-functions.R")
+source("helper_functions/general-helper-functions.R")
+
+# set defaults ------------------------------------------------------------
+
+# for estimates from the data
+alpha_conf <- 0.95 # confidence level, e.g. for confidence intervals
+hRBC_vol <- 90e-12 # human RBC volume: 90e-12 mL
+
+# defaults for the discrete model:
+pmr <- 4 # PMR of parasites sensitive to cabamiquine
+fitn_cost <- 1-7/100 # reduction factor of the PMR of resistant parasites, i.e. the PMR for resistant parasites is pmr*lambda, fitness cost of 7% reduction of PMR
+mut_rate <- 1.05e-9 # mutation rate in base pair substitutions per base pair per generation
+n_res_mut <- 11 # number of mutations to give resistance
+initial_freq_guess <- 2e-8 # initial guess for the frequency of resistant mutants for the data estimates
+init_res_par <- 0 # initial number of resistant parasites
+
+# for plots
+plot_width <- 8
+plot_height <- 6
